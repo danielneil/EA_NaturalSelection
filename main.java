31946a0b3d6810f8b370088935ced3aca5403296
Daniel Neil <danielneilhodder@gmail.com>
2025-06-30 15:52:11 +1000

    Update main.java

--- main.java
+++ main.java
@@ -11,12 +11,23 @@
 # Return to 2
 # Source https://en.wikipedia.org/wiki/Evolutionary_algorithm
 
-
 # "Natural selection is a key mechanism of evolution, first proposed by Charles Darwin. It's a process where organisms 
 # with traits that better enable them to adapt to their environment tend to survive and produce more offspring. 
 # Over generations, these advantageous traits become more common within a population, while disadvantageous ones become less so. 
 # This gradual change in heritable traits over time leads to the diversity of life we see today."
 
-# Considering this framework, we'll establish a predator class that exhibits a preference for "frogs" of a certain color. 
-# The frog class, in turn, will have descendants that evolve to display a specific color, unless that color is favored by the predators. 
-# A UI switch is included to modify the color that the predator favors, thereby demonstrating how the frog population's color distribution is influenced by predation.
+# Based on the provided description, we'll develop a predator class that shows a preference for "frogs" with a 
+# specific color trait. The frog class will subsequently generate offspring that evolve to display a particular color, 
+# inheriting it from their parent with minor variations. If the predator's favored color matches the frog's trait, the predator 
+# will eliminate that frog instance. A user interface toggle is included to manually adjust the color preference of the predators, 
+# thereby vividly demonstrating how the frog population's color distribution is molded by predation.
+
+
+# Main class.
+public class EA_NaturalSelection 
+{
+  public static void main(String[] args) 
+  {
+    EA_NaturalSelection ea = new EA_NaturalSelection();
+  }
+}
