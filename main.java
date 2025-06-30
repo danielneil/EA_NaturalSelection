24fb51a8a37bcd7cd97b6d3fcc9f4e923fba3f54
Daniel Neil <danielneilhodder@gmail.com>
2025-06-30 15:04:21 +1000

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+# Daniel Neil Hodder
+#
+# Randomly generate the initial population of individuals, the first generation.
+# Evaluate the fitness of each individual in the population.
+# Check, if the goal is reached and the algorithm can be terminated.
+# Select individuals as parents, preferably of higher fitness.
+# Produce offspring with optional crossover (mimicking reproduction).
+# Apply mutation operations on the offspring.
+# Select individuals preferably of lower fitness for replacement with new individuals (mimicking natural selection).
+# Return to 2
+#
+# Source: https://en.wikipedia.org/wiki/Evolutionary_algorithm
