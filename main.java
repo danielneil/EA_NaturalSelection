2fba81eebc6a8fd9358042677dbc1c6fa1009c53
Daniel Neil <danielneilhodder@gmail.com>
2025-06-30 15:39:31 +1000

    Update main.java

--- main.java
+++ main.java
@@ -9,16 +9,14 @@
 # Apply mutation operations on the offspring.
 # Select individuals preferably of lower fitness for replacement with new individuals (mimicking natural selection).
 # Return to 2
+# Source https://en.wikipedia.org/wiki/Evolutionary_algorithm
 
-# https://en.wikipedia.org/wiki/Evolutionary_algorithm
-# https://en.wikipedia.org/wiki/Evolutionary_programming
 
 # "Natural selection is a key mechanism of evolution, first proposed by Charles Darwin. It's a process where organisms 
 # with traits that better enable them to adapt to their environment tend to survive and produce more offspring. 
 # Over generations, these advantageous traits become more common within a population, while disadvantageous ones become less so. 
 # This gradual change in heritable traits over time leads to the diversity of life we see today."
 
-# With the above in mind, we'll have a predator class which favours "frogs" of a specific color, and a frog class which
-# it decendents evolve to be of a certain colour, unless that colour is favoured by predators. 
-# A toggle is provided that can be used to adjust the colour that the predator favours, which will show
-# the population of frog's colour be influenced by who gets eaten by the predator. 
+# Considering this framework, we'll establish a predator class that exhibits a preference for "frogs" of a certain color. 
+# The frog class, in turn, will have descendants that evolve to display a specific color, unless that color is favored by the predators. 
+# A UI switch is included to modify the color that the predator favors, thereby demonstrating how the frog population's color distribution is influenced by predation.
