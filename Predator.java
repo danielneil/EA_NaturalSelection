f05b1a926850faf8ccbc94f05ba6b73d6c863124
Daniel Neil <danielneilhodder@gmail.com>
2025-07-02 14:57:37 +1000

    Update Predator.java

--- Predator.java
+++ Predator.java
@@ -1,6 +1,19 @@
 class Predator
 {
+  private BitSet foodColourPreference;
+  
   public Predator()
   {
+    colourPreference = new BitSet(1024);
+  }
+
+  public BitSet getFoodColourPreference()
+  {
+    return foodColourPreference;
+  }
+
+  public void setFoodColourPreference(BitSet _preference)
+  {
+    foodColourPreference = _preference;
   }
 }
